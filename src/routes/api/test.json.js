34654b56f0e3162d3f60d00803926f0b39f8c82b
CaosExa <crassnic@web.de>
2022-06-17 11:11:40 +0200

    1 file modified

--- src/routes/api/test.json.js
+++ src/routes/api/test.json.js
@@ -2,24 +2,24 @@ export async function get({ params }) {
 	// `params.id` comes from [id].js
 	const options = {
 		mode: 'cors', // no-cors, *cors, same-origin,
-			cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-			credentials: 'same-origin',
-			headers: {
-				'Content-Type': 'application/json'
-				// 'Content-Type': 'application/x-www-form-urlencoded',
-			},
-			redirect: 'follow', // manual, *follow, error
-			referrerPolicy: 'no-referrer'
-	}
+		cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+		credentials: 'same-origin',
+		headers: {
+			'Content-Type': 'application/json'
+			// 'Content-Type': 'application/x-www-form-urlencoded',
+		},
+		redirect: 'follow', // manual, *follow, error
+		referrerPolicy: 'no-referrer'
+	};
 	const item = 'Nico'; //await db.get(params.id);
-  const url =
+	const url =
 		'https://es-api.drankdozijn.nl/sale-products?country=DE&language=de&cacheKey=1&premiumMember=N';
 	const proxy = 'https://cors-anywhere.herokuapp.com/';
 	const res = await fetch(url);
 	const data = await res.json();
 	console.log(data);
-		return {body:{data}}
-	}
+	return { body: { data } };
+
 	// if (item) {
 	// 	return {
 	// 		body: { item }
