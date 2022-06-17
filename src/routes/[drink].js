35ff8af3e9a2740ec5959a7a09931351e1404138
CaosExa <crassnic@web.de>
2022-06-17 13:05:00 +0200

    1 file modified

--- src/routes/[drink].js
+++ src/routes/[drink].js
@@ -1,7 +1,7 @@
 import { offerList } from '../stores';
-export async function get({ params }) {
-	const res = await fetch('/api/test.json');
-	const offerList = await res.json()
+export function get({ params }) {
+	// const res = await fetch('/api/test.json');
+	// const offerList = await res.json()
 	const filteredOfferList = offerList.filter((offer) => offer.productGroup.alias === params.drink);
 	return {
 		body: { filteredOfferList }
