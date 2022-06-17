08bebd085410e4c64652af0316e3ce657617e8c9
CaosExa <crassnic@web.de>
2022-06-17 13:10:25 +0200

    1 file added, 2 deleted

--- src/routes/[dri.js
+++
@@ -1,9 +0,0 @@
-import { offerList } from '../stores';
-export function get({ params }) {
-	// const res = await fetch('/api/test.json');
-	// const offerList = await res.json()
-	const filteredOfferList = offerList.filter((offer) => offer.productGroup.alias === params.drink);
-	return {
-		body: { filteredOfferList }
-	};
-}
---
+++ src/routes/dri.js
@@ -0,0 +1,9 @@
+// import { offerList } from '../stores';
+// export function get({ params }) {
+// 	// const res = await fetch('/api/test.json');
+// 	// const offerList = await res.json()
+// 	const filteredOfferList = offerList.filter((offer) => offer.productGroup.alias === params.drink);
+// 	return {
+// 		body: { filteredOfferList }
+// 	};
+// }
