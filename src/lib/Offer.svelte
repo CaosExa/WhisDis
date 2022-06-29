842827dfa45aeaf0846ffde0c09b88416cfb5441
CaosExa <crassnic@web.de>
2022-06-29 14:47:43 +0200

    1 file modified

--- src/lib/Offer.svelte
+++ src/lib/Offer.svelte
@@ -6,53 +6,61 @@
 </script>
 
 <li>
-  <span class="abs-disc">-{(offer.price-offer.salePrice).toFixed(2)}€</span>
-  <span class="rel-disc">{ getRelativeDiscount(offer).toFixed(2)} % </span>
+	<div>
+		<span class="abs-disc">-{(offer.price-offer.salePrice).toFixed(2)}€</span>
+    <span class="rel-disc">{ getRelativeDiscount(offer).toFixed(2)} % </span>
+	</div>
+	
   {#if offer.structuredData}
     <img src={imgRoot + offer.products[0].images[0]} alt={offer.saleDescription} />
   {:else if offer.products.length > 1}
 		<div class="multi-img-container">
-      {#each offer.products as product}
-        <img src={imgRoot + product.images[0]} alt={offer.saleDescription} />
-      {/each}
+			{#each offer.products as product}
+				 <img src={imgRoot + product.images[0]} alt={offer.saleDescription} />
+			{/each}
 		</div>
   {:else}
     <div style="display:flex; justify-content: center; margin: 3em ">No Image</div>
   {/if}
-  <div class="card-footer">
-    <a href={"https://drankdozijn.de/artikel/"+offer.products[0].alias}>
-      {offer.saleDescription} 
-    </a>
-    <b>{offer.salePrice.toFixed(2)}€</b> 
-    <span class="old-price">{offer.price.toFixed(2)}€</span> 
-  </div>
+
+	<div class="card-footer">
+		<a href={"https://drankdozijn.de/artikel/"+offer.products[0].alias}>
+    	{offer.saleDescription} 
+  	</a>
+		<div class="card-pricing">
+			<span><strong>{offer.salePrice.toFixed(2)}€</strong></span>
+  		<span class="old-price">{offer.price.toFixed(2)}€</span> 
+		</div>
+	</div>
+
 </li>
 
 <style>
 .multi-img-container{
-		display:flex;
-		align-items: center;
-		justify-content: center;
-		margin: 1.5em auto -1.5em auto;
-	}
-	.multi-img-container > img{
-		overflow: hidden;
-	}
+  max-width: 100%;
+  display:flex;
+  align-items: center;
+  justify-content: center;
+  margin: 1.5em auto -1.5em auto; 
+}
+.multi-img-container > img{
+  overflow: hidden;
+}
 li > img{
-  max-height: 6rem;
+  max-height: 7rem;
   display: block;
   margin: 1em auto -1.5em auto;
 }
 li {
-    border-radius: 0.3em;
-    background-color: white;
-    padding: 0 1rem;
-    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-    position: relative;
-    overflow: hidden;
-  }
-  .card-footer{
-
+  border-radius: 0.3em;
+  background-color: white;
+  padding: 0 1rem;
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  position: relative;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   }
 a {
   margin-top: 2em;
@@ -77,9 +85,12 @@ a {
   border-radius: 0.3em 0 ;
   left: 0;
 }
-
+.card-pricing{
+	display: flex;
+	justify-content: space-between;
+	margin-bottom: 0.5em;
+}
 .old-price{
-  margin-left: 2em;
   text-decoration: line-through;
   font-size: 0.9em;
 }
