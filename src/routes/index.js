ddf7bb53320047824fe1f1f22d7cd241604fb438
CaosExa <crassnic@web.de>
2022-07-07 13:33:26 +0200

    1 file modified

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,30 +1,30 @@
 
-// export async function get({ url }) {
-// 	const res = await fetch(
-// 		'https://es-api.drankdozijn.nl/sale-products?country=DE&language=de&cacheKey=1&premiumMember=N'
-// 	);
-// 	let offers = await res.json();
-// 	if (offers) {
-// 		offerList.set(offers);
-// 		let data = offers;
-// 		if (url.searchParams.has('drink')) {
-// 			data = offers.filter((offer) => offer.productGroup.alias === url.searchParams.get('drink'));
-// 		}
-// 		return {
-// 			body: { data }
-// 		};
-// 	}
-// 	return {
-// 		status: 404
-// 	};
-// }
-
-import offers from '$lib/test.json';
-console.log('ext fetch');
 export async function get({ url }) {
-	let data = offers;
-	if (url.searchParams.has('drink')) {
-		data = offers.filter((offer) => offer.productGroup.alias === url.searchParams.get('drink'));
+	const res = await fetch(
+		'https://es-api.drankdozijn.nl/sale-products?country=DE&language=de&cacheKey=1&premiumMember=N'
+	);
+	let offers = await res.json();
+	if (offers) {
+		offerList.set(offers);
+		let data = offers;
+		if (url.searchParams.has('drink')) {
+			data = offers.filter((offer) => offer.productGroup.alias === url.searchParams.get('drink'));
+		}
+		return {
+			body: { data }
+		};
 	}
-	return { body: { data } };
+	return {
+		status: 404
+	};
 }
+
+// import offers from '$lib/test.json';
+// console.log('ext fetch');
+// export async function get({ url }) {
+// 	let data = offers;
+// 	if (url.searchParams.has('drink')) {
+// 		data = offers.filter((offer) => offer.productGroup.alias === url.searchParams.get('drink'));
+// 	}
+// 	return { body: { data } };
+// }
